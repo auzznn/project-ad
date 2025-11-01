c9b5c908de9091ddc1fac0332f7e225bb1463fea
miftahr149 <miftahr149@gmail.com>
2025-11-01 13:41:38 +0800

    set constant for path name

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import TokenView, TokenRefreshView
-
+from .const import TOKEN_OBTAIN_NAME, TOKEN_REFRESH_NAME
 
 urlpatterns = [
-  path("token/", TokenView.as_view(), name="obtain_token"),
-  path("token/refresh", TokenRefreshView.as_view(), name="")
+  path("token/", TokenView.as_view(), name=TOKEN_OBTAIN_NAME),
+  path("token/refresh", TokenRefreshView.as_view(), name=TOKEN_REFRESH_NAME)
 ]
