bc9bde7c43fbfa44f63f2551376e780240c522bb
miftahr149 <miftahr149@gmail.com>
2025-11-02 15:56:57 +0800

    add new model to the app 'authentication'
    add model "Student" and "Classroom"

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -10,3 +10,12 @@ class MyUser(AbstractUser):
   ]
 
   role = models.CharField(max_length=20, choices=ROLE_CHOICE, default="student")
+
+class Classroom(models.Model):
+  name = models.CharField(max_length=10, blank=False)
+  supervisor = models.CharField(blank=True)
+
+class Student(models.Model):
+  user = models.OneToOneField(MyUser, on_delete=models.CASCADE, blank=False)
+  class_room = models.ForeignKey(Classroom, on_delete=models.CASCADE, blank=False)
+  qr_url = models.URLField()
