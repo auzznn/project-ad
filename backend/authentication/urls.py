7ae1addd49b6c231e015e63565d72af584b9af3a
miftahr149 <miftahr149@gmail.com>
2025-11-02 16:03:19 +0800

    add urls to for user API

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,8 +1,21 @@
-from django.urls import path
-from .views import TokenView, TokenRefreshView
-from .const import TOKEN_OBTAIN_NAME, TOKEN_REFRESH_NAME
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+  TokenView, 
+  TokenRefreshView,
+  MyUserView,
+)
+from .const import (
+  TOKEN_OBTAIN_PATH_NAME, 
+  TOKEN_REFRESH_PATH_NAME, 
+  USER_PATH_NAME
+)
+
+router = DefaultRouter()
+router.register(r"user", MyUserView, basename=USER_PATH_NAME)
 
 urlpatterns = [
-  path("token/", TokenView.as_view(), name=TOKEN_OBTAIN_NAME),
-  path("token/refresh", TokenRefreshView.as_view(), name=TOKEN_REFRESH_NAME)
+  path("token", TokenView.as_view(), name=TOKEN_OBTAIN_PATH_NAME),
+  path('token/refresh', TokenRefreshView.as_view(), name=TOKEN_REFRESH_PATH_NAME),
+  path('', include(router.urls))
 ]
