2f220aba021460f84b4d1d5c9b32cdbd2ff4e688
miftahr149 <miftahr149@gmail.com>
2025-11-10 15:13:59 +0800

    apply django cors headers middleware

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -141,3 +142,7 @@ REST_FRAMEWORK = {
 }
 
 SITE_ID = 1
+
+CORS_ALLOWED_ORIGINS = [
+  'http://localhost:3000'
+]
