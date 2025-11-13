d98fa9e0573f749ff10bdb03307bf9162f676b6c
miftahr149 <miftahr149@gmail.com>
2025-11-13 12:25:34 +0800

    change the database from sqlite to MySQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,8 +85,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eduqr_db',
+        'USER': 'root',
+        'PASSWORD': 'admin123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
     }
 }
 
