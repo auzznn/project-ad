2e7b10f9c3a3170447997dcfaba3d650ab2e9e1b
miftahr149 <miftahr149@gmail.com>
2025-11-21 01:36:54 +0800

    creating the serializer
    - Create new StudentAttendanceSerializer
    - Create new StudentSerializer

--- backend/authentication/serializer.py
+++ backend/authentication/serializer.py
@@ -1,4 +1,5 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.reverse import reverse
 from django.contrib.sites.models import Site
@@ -43,3 +44,15 @@ class MyUserCreateSerializer(ModelSerializer):
       student_instance = Student.objects.create(**student_instance_data)
       student_instance.save()
     return instance
+
+class StudentSerializer(ModelSerializer):
+  fullname = serializers.SerializerMethodField()
+  student_id = serializers.IntegerField(source="user.id")
+  class_room = serializers.CharField(source="class_room.name")
+  
+  class Meta:
+    model = Student
+    fields =  ["student_id", "fullname", "class_room"]
+  
+  def get_fullname(self, obj: Student) -> str:
+    return f"{obj.user.first_name} {obj.user.last_name}"
