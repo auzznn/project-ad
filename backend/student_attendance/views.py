80908e86b92e1a93112832d8b328787cd7fc6386
miftahr149 <miftahr149@gmail.com>
2025-11-21 01:37:43 +0800

    Create API endpoint for student attendance
    create API point for retrieving daily student attendance

--- backend/student_attendance/views.py
+++ backend/student_attendance/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.http.request import HttpRequest
+from django.db.models.manager import BaseManager
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializer import StudentAttendanceSerializer
+from .models import StudentAttendance
 
 # Create your views here.
+class StudentAttendanceViewSet(viewsets.ModelViewSet):
+  queryset = StudentAttendance.objects.all()
+  serializer_class = StudentAttendanceSerializer
+
+  @action(detail=False, methods=["get"])
+  def daily(self, request: HttpRequest):
+    queryset = self.get_queryset()
+    queryset = queryset.filter(created_at__date=timezone.now())
+    
+    serializer = self.serializer_class(queryset, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
