d36784edb539326007a12413bfc2faf5d1593f8e
fathan <fathanauzanasykur@gmail.com>
2025-11-25 13:54:58 +0800

    remove backend folder

--- backend/base/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-  path("authentication/", include("authentication.urls"))
-]
--- backend/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python manage.py makemigrations
-python manage.py migrate
-python manage.py loaddata fixture/dummydata.json
-python manage.py runserver 0.0.0.0:8080
