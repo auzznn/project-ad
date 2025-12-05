b2cf790c8dee60b4840f19a3b4b4c4ba8470e772
miftahr149 <miftahr149@gmail.com>
2025-12-05 09:12:58 +0800

    add model SahsiahType

--- backend/sahsiah/models.py
+++ backend/sahsiah/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class SahsiahType (models.Model):
+  name = models.CharField(max_length=100, blank=False)
+  description = models.CharField(max_length=256, blank=False)
+  points = models.IntegerField(default=0)
+  tag = models.CharField(max_length=50, blank=False)
+
+  def __str__(self) -> str:
+    return self.name
