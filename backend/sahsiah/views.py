c227393f30d64a1c0e15aca2a728c51d6cb32e60
miftahr149 <miftahr149@gmail.com>
2025-12-05 09:21:44 +0800

    Create view SahsiahTypeView

--- backend/sahsiah/views.py
+++ backend/sahsiah/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import SahsiahType
+from .serializer import SahsiahTypeSerializer
 
 # Create your views here.
+class SahsiahTypeView(viewsets.ModelViewSet):
+  queryset = SahsiahType.objects.all()
+  serializer_class = SahsiahTypeSerializer
