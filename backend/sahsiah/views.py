ed632d3c64c035b6139f07219a330d24a593181f
miftahr149 <miftahr149@gmail.com>
2025-12-08 14:58:26 +0800

    add view and serializer for sahsiah record

--- backend/sahsiah/views.py
+++ backend/sahsiah/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
-from .models import SahsiahType
-from .serializer import SahsiahTypeSerializer
+from .models import SahsiahType, SahsiahRecord
+from .serializer import SahsiahTypeSerializer, SahsiahRecordSerializer
 
 # Create your views here.
 class SahsiahTypeView(viewsets.ModelViewSet):
   queryset = SahsiahType.objects.all()
   serializer_class = SahsiahTypeSerializer
+
+class SahsiahRecordView(viewsets.ModelViewSet):
+  queryset = SahsiahRecord.objects.all()
+  serializer_class = SahsiahRecordSerializer
