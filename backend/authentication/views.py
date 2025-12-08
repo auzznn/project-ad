d7957e357491ed775d8f8d777e364e932737676f
miftahr149 <miftahr149@gmail.com>
2025-12-08 17:42:21 +0800

    modify view UserView

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,7 @@
+from django.db.models import Q
+
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated 
 from rest_framework_simplejwt.views import (
   TokenObtainPairView,
@@ -5,11 +9,12 @@ from rest_framework_simplejwt.views import (
 )
 from rest_framework.viewsets import ModelViewSet
 
-from .models import MyUser
+from .models import MyUser, Student
 from .serializer import (
   MyTokenObtenPairSerializer,
   MyUserCreateSerializer,
-  MyUserRetrieveSerializer
+  MyUserRetrieveSerializer,
+  StudentSerializer
 )
 
 # Create your views here.
@@ -22,3 +27,15 @@ class MyUserView(ModelViewSet):
 
   def get_serializer_class(self):
     return MyUserCreateSerializer if self.action == 'create' else MyUserRetrieveSerializer
+
+  def list(self, request, *args, **kwargs):
+    queryset = self.get_queryset()
+    non_student_user = queryset.filter(~Q(role='student'))
+    student_id = queryset.filter(role='student').values_list('id')
+    students = Student.objects.filter(user__in=student_id)
+
+    non_student_user_serializer = MyUserRetrieveSerializer(instance=non_student_user, many=True)
+    student_user_serializer = StudentSerializer(instance=students, many=True)
+    merged_data = non_student_user_serializer.data + student_user_serializer.data
+
+    return Response(merged_data, status=status.HTTP_200_OK)
