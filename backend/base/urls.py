39dd3065e200808f3589620129b19abd326de912
miftahr149 <miftahr149@gmail.com>
2025-12-08 14:56:49 +0800

    update the routing for sahsiah

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 urlpatterns = [
   path("authentication/", include("authentication.urls")),
   path("student_attendance/", include("student_attendance.urls")),
-  path("sahsiah", include("sahsiah.urls"))
+  path("sahsiah/", include("sahsiah.urls"))
 ]
--- backend/sahsiah/urls.py
+++ backend/sahsiah/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import SahsiahTypeView
+from .views import SahsiahTypeView, SahsiahRecordView
 
 router = DefaultRouter()
-router.register("/type", SahsiahTypeView)
+router.register("type", SahsiahTypeView)
+router.register("record", SahsiahRecordView)
 
 urlpatterns = [
   path("", include(router.urls))
