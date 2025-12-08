e497ee07dbb4adb02b2d0765480c01d290a0fcc1
miftahr149 <miftahr149@gmail.com>
2025-12-08 14:57:28 +0800

    Add model SahsiahRecord

--- backend/sahsiah/models.py
+++ backend/sahsiah/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from authentication.models import Student
 
 # Create your models here.
-class SahsiahType (models.Model):
+class SahsiahType(models.Model):
   name = models.CharField(max_length=100, blank=False)
   description = models.CharField(max_length=256, blank=False)
   points = models.IntegerField(default=0)
@@ -9,3 +11,8 @@ class SahsiahType (models.Model):
 
   def __str__(self) -> str:
     return self.name
+
+class SahsiahRecord(models.Model):
+  student_id = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='sahsiah')
+  sahsiah_type = models.ForeignKey(SahsiahType, on_delete=models.CASCADE)
+  timestamp = models.DateTimeField(default=timezone.now, blank=False)
