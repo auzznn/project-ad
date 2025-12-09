d9fa8f5bff1c2513f32662f0910c707b3999040d
miftahr149 <miftahr149@gmail.com>
2025-12-09 12:22:18 +0800

    update the models RMTRecord

--- backend/rmt/models.py
+++ backend/rmt/models.py
@@ -11,6 +11,6 @@ class RMTRecord(models.Model):
     now_kl = timezone.now().astimezone(kl_tz)
     return now_kl.replace(hour=0, minute=0, second=0, microsecond=0)
 
-  student = models.ForeignKey(Student, blank=False, null=False, on_delete=models.CASCADE)
+  student_id = models.ForeignKey(Student, blank=False, null=False, on_delete=models.CASCADE)
   created_at = models.DateTimeField(default=default_datetime)
   updated_at = models.DateTimeField(default=default_datetime)
