52e2430df12e2e6340ae956aaa86bf2946548fc4
miftahr149 <miftahr149@gmail.com>
2025-12-11 12:41:39 +0800

    register model SahsiahRecord and SahsiahType to admin

--- backend/sahsiah/admin.py
+++ backend/sahsiah/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import SahsiahRecord, SahsiahType
 
 # Register your models here.
+admin.site.register(SahsiahRecord)
+admin.site.register(SahsiahType)
