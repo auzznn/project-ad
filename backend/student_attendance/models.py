ec24f22e5ca72f6a3e86497d12c20519622b0956
miftahr149 <miftahr149@gmail.com>
2025-12-17 22:39:15 +0800

    refactor model StudentAttendance

--- backend/student_attendance/models.py
+++ backend/student_attendance/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from authentication.models import Student
 from datetime import time
+from . import const
+
 from django.conf import settings
 import pytz
 
@@ -14,23 +16,17 @@ class StudentAttendance(models.Model):
   ON_TIME_CODE = "on-time"
   ABSENT_CODE = "absent"
   
-  STATUS_ATTENDANCE = [
-    (ABSENT_CODE, "Absent"), 
-    (ON_TIME_CODE, "On time"), 
-    ("late", "Late")
-  ]
-  
   def default_datetime():
     now_kl = set_timezone(timezone.now())
     return now_kl.replace(hour=0, minute=0, second=0, microsecond=0)
   
-  DEFAULT_STATUS = "absent"
+  DEFAULT_STATUS = const.ABSENT_STATUS_KEY
   ON_TIME = time(hour=7, minute=40)
   ABSENT_TIME = default_datetime().time()
 
   
   student_id = models.ForeignKey(Student, related_name="attendance", on_delete=models.CASCADE)
-  status = models.CharField(choices=STATUS_ATTENDANCE, default=DEFAULT_STATUS, max_length=15)
+  status = models.CharField(choices=const.STUDENT_ATTENDANCE_STATUS, default=DEFAULT_STATUS, max_length=15)
   date = models.DateField(default=timezone.now)
   timestamp = models.DateTimeField(default=default_datetime)
   note = models.TextField(blank=True, null=True)
@@ -40,8 +36,8 @@ class StudentAttendance(models.Model):
 
   def save(self, *args, **kwargs):
     if set_timezone(self.timestamp).time() == self.ABSENT_TIME:
-      self.status = self.ABSENT_CODE
+      self.status = const.ABSENT_STATUS_KEY
       return super().save(*args, **kwargs)  
-    status_index = 2 if self.timestamp.time() > self.ON_TIME else 1
-    self.status = self.STATUS_ATTENDANCE[status_index][0]
+    new_status = const.LATE_STATUS_KEY if self.timestamp.time() > self.ON_TIME else const.ON_TIME_STATUS_KEY
+    self.status = new_status
     return super().save(*args, **kwargs)
