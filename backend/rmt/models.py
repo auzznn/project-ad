2bf10bae5c1ca55c3642b76af025b196ecd46a08
miftahr149 <miftahr149@gmail.com>
2025-12-19 11:49:32 +0800

    add field migrate_student_id to RMTRecord

--- backend/rmt/models.py
+++ backend/rmt/models.py
@@ -2,15 +2,24 @@ from django.db import models
 import pytz
 from django.utils import timezone
 
-from authentication.models import Student
+from authentication.models import Student, MigrateStudent
+
 
 # Create your models here.
 class RMTRecord(models.Model):
-  def default_datetime():
-    kl_tz = pytz.timezone('Asia/Kuala_Lumpur')
-    now_kl = timezone.now().astimezone(kl_tz)
-    return now_kl.replace(hour=0, minute=0, second=0, microsecond=0)
+    def default_datetime():
+        kl_tz = pytz.timezone("Asia/Kuala_Lumpur")
+        now_kl = timezone.now().astimezone(kl_tz)
+        return now_kl.replace(hour=0, minute=0, second=0, microsecond=0)
 
-  student_id = models.ForeignKey(Student, blank=False, null=False, on_delete=models.CASCADE)
-  created_at = models.DateTimeField(default=default_datetime)
-  updated_at = models.DateTimeField(default=default_datetime)
+    student_id = models.ForeignKey(
+        Student, blank=False, null=False, on_delete=models.CASCADE
+    )
+    migrate_student_id = models.ForeignKey(
+        MigrateStudent,
+        related_name="migrate_rmt",
+        on_delete=models.CASCADE,
+        null=True,
+    )
+    created_at = models.DateTimeField(default=default_datetime)
+    updated_at = models.DateTimeField(default=default_datetime)
