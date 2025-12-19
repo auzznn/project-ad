8a549bc0dec380d6aae5e1fff39e32e15874e576
miftahr149 <miftahr149@gmail.com>
2025-12-19 10:24:34 +0800

    create temporary student table MigrateStudent

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -12,84 +12,155 @@ import qrcode as qr
 import string
 import random
 
+
 # Create your models here.
 class MyUser(AbstractUser):
-  ROLE_CHOICE = [
-    ('admin', 'admin'),
-    ('teacher', 'teacher'),
-    ('student', 'student'),
-    ('parent', 'parent')
-  ]
+    ROLE_CHOICE = [
+        ("admin", "admin"),
+        ("teacher", "teacher"),
+        ("student", "student"),
+        ("parent", "parent"),
+    ]
 
-  role = models.CharField(max_length=20, choices=ROLE_CHOICE, default="student")
+    role = models.CharField(max_length=20, choices=ROLE_CHOICE, default="student")
 
-  @property
-  def fullname(self) -> str:
-    return f"{self.first_name} {self.last_name}"
-  
+    @property
+    def fullname(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
 class Classroom(models.Model):
-  
-  def default_section() -> str:
-    LENGTH = 15
-    characters = string.ascii_letters
 
-    random_section_name = ''.join(random.choice(characters) for i in range(LENGTH))    
-    return random_section_name
-  
-  grade = models.IntegerField(
-    blank=False, null=False, default=1, validators=[
-    MinValueValidator(1, 'class grade can\'t be below 1'),
-    MaxValueValidator(6, 'class grade can\'t be above 6')
-  ])
-  class_section = models.CharField(max_length=50, blank=False, null=False, default=default_section)
-  
-  supervisor = models.CharField(blank=True, null=True, max_length=50)
+    def default_section() -> str:
+        LENGTH = 15
+        characters = string.ascii_letters
 
-  def __str__(self) -> str:
-    return f'{self.grade}{self.class_section}'
-  
-  @property
-  def name(self) -> str:
-    return self.__str__()
+        random_section_name = "".join(random.choice(characters) for i in range(LENGTH))
+        return random_section_name
+
+    grade = models.IntegerField(
+        blank=False,
+        null=False,
+        default=1,
+        validators=[
+            MinValueValidator(1, "class grade can't be below 1"),
+            MaxValueValidator(6, "class grade can't be above 6"),
+        ],
+    )
+    class_section = models.CharField(
+        max_length=50, blank=False, null=False, default=default_section
+    )
+
+    supervisor = models.CharField(blank=True, null=True, max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.grade}{self.class_section}"
+
+    @property
+    def name(self) -> str:
+        return self.__str__()
+
 
 class Student(models.Model):
-  QR_IMAGE_FORMAT = "jpeg"
-  
-  user = models.OneToOneField(MyUser, on_delete=models.CASCADE, primary_key=True, related_name='student')
-  class_room = models.ForeignKey(Classroom, on_delete=models.CASCADE, blank=True, null=True, related_name='student')
-  qr_code = models.ImageField(upload_to='qrcodes/', blank=True, null=True)
-  rmt_elligible = models.BooleanField(default=False, blank=False, null=True)
-  date_of_birth = models.DateTimeField(blank=False, null=False, default=timezone.now)
+    QR_IMAGE_FORMAT = "jpeg"
 
-  @classmethod
-  def generate_qr_image(cls, url_link: str) -> File:
-    
-    qr_image = qr.make(url_link)
-    buffer = BytesIO()
-    qr_image.save(buffer, format=cls.QR_IMAGE_FORMAT)
-    buffer.seek(0)
-    return File(buffer, name=f"qr.{cls.QR_IMAGE_FORMAT}")
+    user = models.OneToOneField(
+        MyUser, on_delete=models.CASCADE, primary_key=True, related_name="student"
+    )
+    class_room = models.ForeignKey(
+        Classroom,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="student",
+    )
+    qr_code = models.ImageField(upload_to="qrcodes/", blank=True, null=True)
+    rmt_elligible = models.BooleanField(default=False, blank=False, null=True)
+    date_of_birth = models.DateTimeField(blank=False, null=False, default=timezone.now)
 
-  def generate_qr(self) -> None:
-    SITE_DOMAIN = Site.objects.get_current().domain
-    URL_PATH = reverse(USER_DETAIL_PATH_NAME, args=[self.user.pk])
-    STUDENT_DATA_URL = f'http://{SITE_DOMAIN}{URL_PATH}'
-    
-    qr_file = self.generate_qr_image(STUDENT_DATA_URL)
-    self.qr_code.save(f'{self.user.username}.{self.QR_IMAGE_FORMAT}', qr_file, save=False)
+    @classmethod
+    def generate_qr_image(cls, url_link: str) -> File:
 
-  def save(self, *args, **kwargs):
-    if not self.qr_code:
-      self.generate_qr()
-    if self.qr_code and not self.qr_code.storage.exists(self.qr_code.name):
-      self.generate_qr()
-    super().save(*args, **kwargs)
+        qr_image = qr.make(url_link)
+        buffer = BytesIO()
+        qr_image.save(buffer, format=cls.QR_IMAGE_FORMAT)
+        buffer.seek(0)
+        return File(buffer, name=f"qr.{cls.QR_IMAGE_FORMAT}")
 
-  def __str__(self) -> str:
-    return f'{self.user.first_name} {self.user.last_name}' 
-  
-  @property
-  def academic_year(self) -> str:
-    return_value = timezone.now().year
+    def generate_qr(self) -> None:
+        SITE_DOMAIN = Site.objects.get_current().domain
+        URL_PATH = reverse(USER_DETAIL_PATH_NAME, args=[self.user.pk])
+        STUDENT_DATA_URL = f"http://{SITE_DOMAIN}{URL_PATH}"
 
-    return f'{return_value}'
+        qr_file = self.generate_qr_image(STUDENT_DATA_URL)
+        self.qr_code.save(
+            f"{self.user.username}.{self.QR_IMAGE_FORMAT}", qr_file, save=False
+        )
+
+    def save(self, *args, **kwargs):
+        if not self.qr_code:
+            self.generate_qr()
+        if self.qr_code and not self.qr_code.storage.exists(self.qr_code.name):
+            self.generate_qr()
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f"{self.user.first_name} {self.user.last_name}"
+
+    @property
+    def academic_year(self) -> str:
+        return_value = timezone.now().year
+
+        return f"{return_value}"
+
+
+class MigrateStudent(models.Model):
+    QR_IMAGE_FORMAT = "jpeg"
+
+    class_room = models.ForeignKey(
+        Classroom,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="migrate_student",
+    )
+    qr_code = models.ImageField(upload_to="qrcodes/", blank=True, null=True)
+    rmt_elligible = models.BooleanField(default=False, blank=False, null=True)
+    date_of_birth = models.DateTimeField(blank=False, null=False, default=timezone.now)
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+
+    @classmethod
+    def generate_qr_image(cls, url_link: str) -> File:
+
+        qr_image = qr.make(url_link)
+        buffer = BytesIO()
+        qr_image.save(buffer, format=cls.QR_IMAGE_FORMAT)
+        buffer.seek(0)
+        return File(buffer, name=f"qr.{cls.QR_IMAGE_FORMAT}")
+
+    def generate_qr(self) -> None:
+        SITE_DOMAIN = Site.objects.get_current().domain
+        URL_PATH = reverse(USER_DETAIL_PATH_NAME, args=[self.user.pk])
+        STUDENT_DATA_URL = f"http://{SITE_DOMAIN}{URL_PATH}"
+
+        qr_file = self.generate_qr_image(STUDENT_DATA_URL)
+        self.qr_code.save(
+            f"{self.user.username}.{self.QR_IMAGE_FORMAT}", qr_file, save=False
+        )
+
+    def save(self, *args, **kwargs):
+        if not self.qr_code:
+            self.generate_qr()
+        if self.qr_code and not self.qr_code.storage.exists(self.qr_code.name):
+            self.generate_qr()
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f"{self.user.first_name} {self.user.last_name}"
+
+    @property
+    def academic_year(self) -> str:
+        return_value = timezone.now().year
+
+        return f"{return_value}"
