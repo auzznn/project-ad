4b9d996e5bccf94683f9fa8d8259a3484db007a2
miftahr149 <miftahr149@gmail.com>
2025-12-19 11:29:03 +0800

    add field migrate_student_id to class SahsiahRecord

--- backend/sahsiah/models.py
+++ backend/sahsiah/models.py
@@ -1,18 +1,28 @@
 from django.db import models
 from django.utils import timezone
-from authentication.models import Student
+from authentication.models import Student, MigrateStudent
+
 
 # Create your models here.
 class SahsiahType(models.Model):
-  name = models.CharField(max_length=100, blank=False)
-  description = models.CharField(max_length=256, blank=False)
-  points = models.IntegerField(default=0)
-  tag = models.CharField(max_length=50, blank=False)
+    name = models.CharField(max_length=100, blank=False)
+    description = models.CharField(max_length=256, blank=False)
+    points = models.IntegerField(default=0)
+    tag = models.CharField(max_length=50, blank=False)
 
-  def __str__(self) -> str:
-    return self.name
+    def __str__(self) -> str:
+        return self.name
+
 
 class SahsiahRecord(models.Model):
-  student_id = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='sahsiah')
-  sahsiah_type = models.ForeignKey(SahsiahType, on_delete=models.CASCADE)
-  timestamp = models.DateTimeField(default=timezone.now, blank=False)
+    student_id = models.ForeignKey(
+        Student, on_delete=models.CASCADE, related_name="sahsiah"
+    )
+    migrate_student_id = models.ForeignKey(
+        MigrateStudent,
+        on_delete=models.CASCADE,
+        related_name="migrate_sahsiah",
+        null=True,
+    )
+    sahsiah_type = models.ForeignKey(SahsiahType, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=timezone.now, blank=False)
