b2d2601eaa51a656b3dac5809781dfb93d7b1e61
miftahr149 <miftahr149@gmail.com>
2025-12-21 21:17:31 +0800

    add serializer ClassroomSerializer

--- backend/authentication/serializer.py
+++ backend/authentication/serializer.py
@@ -68,3 +68,7 @@ class CreateStudentSerializer(ModelSerializer):
         model = MigrateStudent
         fields = '__all__'
 
+class ClassroomSerializer(ModelSerializer):
+    class Meta:
+        model = Classroom
+        fields = '__all__'
