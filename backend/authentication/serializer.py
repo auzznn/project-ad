827301c28866640405611b16f5373c5b5184136b
miftahr149 <miftahr149@gmail.com>
2025-12-21 21:18:38 +0800

    update indentation

--- backend/authentication/serializer.py
+++ backend/authentication/serializer.py
@@ -4,28 +4,30 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.reverse import reverse
 from django.contrib.sites.models import Site
 
-from .models import MyUser, Student, Classroom
+from .models import MyUser, Student, MigrateStudent, Classroom
 from .const import TOKEN_REFRESH_PATH_NAME
 
+
 class MyTokenObtenPairSerializer(TokenObtainPairSerializer):
-  
-  @classmethod
-  def get_token(cls, user: MyUser) -> dict[str, any]:
-    domain = Site.objects.get_current().domain
-    token = super().get_token(user)
-    
-    token['first_name'] = user.first_name
-    token['last_name'] = user.last_name
-    token['role'] = user.role
-    token['url'] = {
-      'refresh': f'https://{domain}{reverse(TOKEN_REFRESH_PATH_NAME)}'
-    }
-    return token
+
+    @classmethod
+    def get_token(cls, user: MyUser) -> dict[str, any]:
+        domain = Site.objects.get_current().domain
+        token = super().get_token(user)
+
+        token["first_name"] = user.first_name
+        token["last_name"] = user.last_name
+        token["role"] = user.role
+        token["url"] = {
+            "refresh": f"https://{domain}{reverse(TOKEN_REFRESH_PATH_NAME)}"
+        }
+        return token
+
 
 class MyUserRetrieveSerializer(ModelSerializer):
-  class Meta:
-    model = MyUser
-    fields = ['id', 'fullname']
+    class Meta:
+        model = MyUser
+        fields = ["id", "fullname"]
 
 
 class MyUserCreateSerializer(ModelSerializer):
