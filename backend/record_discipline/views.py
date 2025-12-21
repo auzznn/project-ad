4d125e33174d41b5d4fdf7be8cbd06f2b0a91298
miftahr149 <miftahr149@gmail.com>
2025-12-22 07:32:11 +0800

    refactor leaderboard view in sahsiah and record discipline

--- backend/record_discipline/views.py
+++ backend/record_discipline/views.py
@@ -1,18 +1,14 @@
-from django.shortcuts import render
-from rest_framework import viewsets, status
-from authentication.models import Student
+from rest_framework import viewsets
+from authentication.models import MigrateStudent
 from .models import DisciplineRecord, DisciplineType
 from django.db.models import Sum, Case, When, F, IntegerField
 from django.conf import settings
 from .serializer import (
     DisciplineRecordSerializer,
     DisciplineTypeSerializer,
-    DisciplineLeaderboardSerializer,
 )
-from rest_framework.decorators import action
-from rest_framework.response import Response
 from base.pagination import StandardResultsSetPagination
-
+from base.views import GeneralLeaderboardView
 
 # Create your views here.
 class DisciplineTypeView(viewsets.ModelViewSet):
@@ -27,15 +23,14 @@ class DisciplineRecordView(viewsets.ModelViewSet):
     pagination_class = StandardResultsSetPagination
 
 
-class DisciplineLeaderboardView(viewsets.ReadOnlyModelViewSet):
-    pagination_class = StandardResultsSetPagination
+class DisciplineLeaderboardView(GeneralLeaderboardView):
     queryset = (
-        Student.objects.annotate(
+        MigrateStudent.objects.annotate(
             total_discipline_point=Sum(
                 Case(
                     # Only sum points for records within the academic year
                     When(
-                        sahsiah__timestamp__range=(
+                        discipline__timestamp__range=(
                             settings.ACADEMIC_YEAR_START,
                             settings.ACADEMIC_YEAR_END,
                         ),
@@ -47,62 +42,6 @@ class DisciplineLeaderboardView(viewsets.ReadOnlyModelViewSet):
             )
         )
         .order_by("-total_discipline_point")
-        .select_related("user", "class_room")
+        .select_related("class_room")
     )
-
-    serializer_class = DisciplineLeaderboardSerializer
-
-    def create_ranking_student(self, queryset=None):
-        if not queryset:
-            queryset = self.get_queryset()
-
-        ranked_data = []
-        rank = 0
-        last_points = None
-
-        for i, student in enumerate(queryset):
-            current_points = student.total_discipline_point or 0
-
-            # Use 'dense' ranking: same points get the same rank
-            if current_points != last_points:
-                rank = i + 1
-            last_points = current_points
-
-            # Create a dictionary for the serializer to process
-            ranked_data.append(
-                {
-                    "student_id": student.user.id,
-                    "student_name": student.user.fullname,
-                    "class_room": student.class_room.name,
-                    "discipline_point": current_points,
-                    "ranking": rank,  # ADD THE RANK HERE
-                }
-            )
-
-        serializer = self.get_serializer(ranked_data, many=True)
-        return serializer
-    
-    def create_list_response(self, serializer):
-        page = self.paginate_queryset(serializer.data)
-        if page is not None:
-            return self.get_paginated_response(page)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.create_ranking_student(queryset=queryset)
-        return self.create_list_response(serializer)
-
-    @action(detail=False, url_path=r"(?P<grade>[0-9]+)/(?P<section>[^/.]+)")
-    def group_by_class(self, request, grade: int, section: str, *args, **kwargs):
-        queryset = self.get_queryset().filter(
-            class_room__grade=grade, class_room__class_section__exact=section
-        )
-        serializer = self.create_ranking_student(queryset=queryset)
-        return self.create_list_response(serializer)
-
-    @action(detail=False, url_path=r"(?P<grade>[0-9]+)")
-    def group_by_grade(self, request, grade: int, *args, **kwargs):
-        queryset = self.get_queryset().filter(class_room__grade=grade)
-        serializer = self.create_ranking_student(queryset=queryset)
-        return self.create_list_response(serializer)
+    point_field = "total_discipline_point"
--- backend/sahsiah/serializer.py
+++ backend/sahsiah/serializer.py
@@ -14,11 +14,3 @@ class SahsiahRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = SahsiahRecord
         fields = "__all__"
-
-
-class SahsiahLeaderboardSerializer(serializers.Serializer):
-    student_id = serializers.IntegerField()
-    student_name = serializers.CharField()
-    sahsiah_point = serializers.IntegerField()
-    class_room = serializers.CharField()
-    ranking = serializers.IntegerField()
