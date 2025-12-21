883225bf5dd43c591ecc07b67110ac5eab5aedd8
miftahr149 <miftahr149@gmail.com>
2025-12-21 21:22:37 +0800

    Refactor urls.py in base

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-  path("authentication/", include("authentication.urls")),
-  path("student_attendance/", include("student_attendance.urls")),
-  path("sahsiah/", include("sahsiah.urls")),
-  path("rmt/", include("rmt.urls")),
-  path("discipline/", include('record_discipline.urls'))
+  path("authentication/", include(("authentication.urls", "authentication"))),
+  path("student_attendance/", include(("student_attendance.urls", "student_attendance"))),
+  path("sahsiah/", include(("sahsiah.urls", "sahsiah"))),
+  path("rmt/", include(("rmt.urls", "rmt"))),
+  path("discipline/", include(("record_discipline.urls", "record_discipline")))
 ]
