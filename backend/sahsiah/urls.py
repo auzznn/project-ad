e4bdba825d29d058d6d94e401e88b37fc4516f35
miftahr149 <miftahr149@gmail.com>
2025-12-28 08:34:15 +0800

    update router

--- backend/sahsiah/urls.py
+++ backend/sahsiah/urls.py
@@ -4,12 +4,14 @@ from .views import (
   SahsiahTypeView, 
   SahsiahRecordView, 
   SahsiahLeaderboardView,
+  SahsiahAnalyticsViewSet
 )
 
 sahsiah_router = DefaultRouter()
 sahsiah_router.register("type", SahsiahTypeView)
 sahsiah_router.register("record", SahsiahRecordView)
 sahsiah_router.register("leaderboard", SahsiahLeaderboardView) 
+sahsiah_router.register('statistic', SahsiahAnalyticsViewSet, basename='statisitc')
 
 urlpatterns = [
   path("", include(sahsiah_router.urls))
