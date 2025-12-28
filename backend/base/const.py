738324e3e3b24247bad30e11d19846ea437852ab
miftahr149 <miftahr149@gmail.com>
2025-12-28 09:21:35 +0800

    format base.const

--- backend/base/const.py
+++ backend/base/const.py
@@ -3,15 +3,35 @@ STUDENT_BASENAME_PATH = "student"
 CLASSROOM_BASENAME_PATH = "classroom"
 USER_BASENAME_PATH = "user"
 
-#Student Attendance
-GENERAL_STUDENT_ATTENDANCE_BASENAME_PATH = "student-attendance" 
+# Student Attendance
+GENERAL_STUDENT_ATTENDANCE_BASENAME_PATH = "student-attendance"
 DAILY_STUDENT_ATTENDANCE_BASENAME_PATH = "student-attendance-daily"
 DATE_STUDENT_ATTENDANCE_BASENAME_PATH = "student-attendance-date"
 DAILY_STATISTIC_STUDENT_ATTENDANCE_BASENAME_PATH = "student-attendance-daily-statistic"
-YEARLY_STATISTIC_STUDENT_ATTENDANCE_BASENAME_PATH = "student-attendance-yearly-statistic"
-MONTHLY_STATISTIC_STUDENT_ATTENDANCE_BASENAME_PATH = "student-attendance-monthly-statistic"
-#Sahsiah
+YEARLY_STATISTIC_STUDENT_ATTENDANCE_BASENAME_PATH = (
+    "student-attendance-yearly-statistic"
+)
+MONTHLY_STATISTIC_STUDENT_ATTENDANCE_BASENAME_PATH = (
+    "student-attendance-monthly-statistic"
+)
+# Sahsiah
 
-#Discipline Record
+# Discipline Record
 
-#RMT
+# RMT
+
+# Utility
+MONTH_NAMES = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+]
