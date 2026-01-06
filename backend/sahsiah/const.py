58d207b82870070304572af238d63d48b27278e7
miftahr149 <miftahr149@gmail.com>
2026-01-06 13:40:34 +0800

    Create const.py

---
+++ backend/sahsiah/const.py
@@ -0,0 +1,10 @@
+PUNCTUALITY_NAME = "Punctuality"
+
+CONSTANT_SAHSIAH_TYPE_INFORMATION = {
+    9000: {
+        "name": PUNCTUALITY_NAME,
+        "description": "",
+        "points": 5,
+        "tag": "academic",
+    }
+}
