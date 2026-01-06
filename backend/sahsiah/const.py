b18f6d1aabe6133490bd6fc27963e431523a2fb4
miftahr149 <miftahr149@gmail.com>
2026-01-06 13:56:01 +0800

    add description

--- backend/sahsiah/const.py
+++ backend/sahsiah/const.py
@@ -3,7 +3,7 @@ PUNCTUALITY_NAME = "Punctuality"
 CONSTANT_SAHSIAH_TYPE_INFORMATION = {
     9000: {
         "name": PUNCTUALITY_NAME,
-        "description": "",
+        "description": "Student attend to the school before 7:40 AM",
         "points": 5,
         "tag": "academic",
     }
