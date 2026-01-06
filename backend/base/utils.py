7932dd68cb50ef40fe7529771303728e081db0f7
miftahr149 <miftahr149@gmail.com>
2026-01-06 13:40:12 +0800

    create new util function

--- backend/base/utils.py
+++ backend/base/utils.py
@@ -2,10 +2,17 @@ import pytz
 from django.utils import timezone
 from django.conf import settings
 
+
 def set_timezone(datetime: timezone.datetime):
     tz = pytz.timezone(settings.TIME_ZONE)
     return datetime.astimezone(tz)
 
+
 def default_datetime():
     today = set_timezone(timezone.now())
     return today.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def create_or_update_constant_merit_type(records, merit_model):
+    for merit_id, merit_data in records.items():
+        merit_model.objects.update_or_create(id=merit_id, defaults=merit_data)
