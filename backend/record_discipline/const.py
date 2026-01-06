8e805493ac4858063a45c22789c3b3de74a9353b
miftahr149 <miftahr149@gmail.com>
2026-01-06 13:40:26 +0800

    Create const.py

---
+++ backend/record_discipline/const.py
@@ -0,0 +1,10 @@
+LATE_NAME = "Late to School"
+
+CONSTANT_DISCIPLINE_TYPE_INFORMATION = {
+    9000: {
+        "name": LATE_NAME,
+        "description": "Student arrived after the official start time (7:40 AM) without a valid excuse.",
+        "points": -5,
+        "tag": "Punctuality",
+    }
+}
